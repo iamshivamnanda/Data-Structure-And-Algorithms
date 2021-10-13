0271ed56be9bbfd66425320df5eafd75675b066f
shivamnanda436 <38239120+shivamnanda436@users.noreply.github.com>
2021-10-13 16:44:59 +0530

    BST DONE

--- Tree/BST.java
+++ Tree/BST.java
@@ -1,20 +1,20 @@
 package Tree;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
 
+
+class Pair{
+    Node node;
+    int hd;
+    Pair(Node n,int h){node=n;hd=h;}
+}
 public class BST {
-    ArrayList<Integer> inOrder(Node root) {
-        ArrayList<Integer> mArrayList = new ArrayList<>();
-       if(root == null){
-           return mArrayList;
-       }
-       ArrayList<Integer> lArrayList = inOrder(root.left);
-       mArrayList.addAll(lArrayList);
-       mArrayList.add(root.data);
-       ArrayList<Integer> rArrayList = inOrder(root.right);
-       mArrayList.addAll(rArrayList);
-       return mArrayList;
-    }
+    
     Node insert(Node root, int Key) {
         // if(root == null){
         //     return new Node(Key);
@@ -130,5 +130,227 @@ public class BST {
             }
         }
         return res;
+    }
+    static int count = 0;
+    static int res = -1;
+    public int KthSmallestElement(Node root, int K) {
+        // Write your code here
+       count = K;
+        helper(root);
+        if(count >0){
+            return -1;
+        }
+        return res;
+    }
+    static void helper(Node root){
+         if(root ==null){
+            return;
+        }
+            helper(root.left);
+            count--;
+            if(count==0){
+                res = root.data;
+            }
+            helper(root.right);
+        
+    }
+    boolean isBST(Node root)
+    {
+       return isBSThelper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    static boolean isBSThelper(Node root,int min,int max){
+        if(root == null){
+            return true;
+        }
+        return ( (root.data>min) && (root.data <max) && isBSThelper(root.left, min, root.data) && isBSThelper(root.right, root.data, max) );
+    }
+
+  static  ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> mArrayList = new ArrayList<>();
+       if(root == null){
+           return mArrayList;
+       }
+       ArrayList<Integer> lArrayList = inOrder(root.left);
+       mArrayList.addAll(lArrayList);
+       mArrayList.add(root.data);
+       ArrayList<Integer> rArrayList = inOrder(root.right);
+       mArrayList.addAll(rArrayList);
+       return mArrayList;
+    }
+    public static ArrayList<Integer> findCommon(Node root1,Node root2)
+    {
+        HashSet<Integer> set = new HashSet<>();
+       ArrayList<Integer> list1 = inOrder(root1);
+        for (Integer integer : list1) {
+            set.add(integer);
+        }
+        list1 = inOrder(root2);
+        ArrayList<Integer> mArrayList = new ArrayList<>();
+        for (Integer integer : list1) {
+            if(set.contains(integer)){
+                mArrayList.add(integer);
+            }
+        }
+        return mArrayList;
+    }
+    Node LCA(Node root, int n1, int n2)
+	{
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        } 
+        Node lLCNode = LCA(root.left, n1, n2);
+        Node rLCNode = LCA(root.right, n1, n2);
+        if(lLCNode == null){
+            return rLCNode;
+        }else if(rLCNode == null){
+            return lLCNode;
+        }else{
+            return root;
+        }
+    }
+
+    public Node correctBST(Node root)
+    {
+        ArrayList<Node> res = new ArrayList<>();
+        res.add(null);
+        res.add(null);
+        res.add(null);
+        correctBSTHelper(root,res);
+            int temp = res.get(1).data;
+            res.get(1).data = res.get(2).data;
+            res.get(2).data = temp;
+        return root;
+    }
+    static public void correctBSTHelper(Node root,ArrayList<Node> res){
+        if(root == null){
+            return;
+        }
+        correctBSTHelper(root.left,res);
+        if(res.get(0) != null && root.data <res.get(0).data){
+            if(res.get(1) == null){
+                res.set(1, res.get(0));
+            }
+            res.set(2, root);
+        }
+        res.set(0, root);
+        correctBSTHelper(root.right,res);
+    }
+    static boolean findPair(Node root, int X) 
+    {
+        HashSet<Integer> set =new HashSet<>();
+        return findPairhelper(root, X, set);
+    }
+    static boolean findPairhelper(Node root,int x,HashSet<Integer> s){
+        if(root == null){
+            return false;
+        }
+        if(findPairhelper(root.left, x, s) == true){
+            return true;
+        }
+        if(s.contains(x-root.data)){
+            return true;
+        }else{
+            s.add(root.data);
+        }
+        if(findPairhelper(root.right, x, s) == true){
+            return true;
+        }
+        return false;
+    }
+    public ArrayList <Integer> verticalSum(Node root) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        verticalSumHelper(root, 0, map);
+        for (Map.Entry sum : map.entrySet()) {
+            list.add((int) sum.getValue());
+        }
+        return list;
+    }
+    static  void verticalSumHelper(Node root,int hd,TreeMap<Integer,Integer> map){
+        if(root == null){
+            return;
+        }
+        verticalSumHelper(root.left, hd-1, map);
+        int psum = (map.get(hd) == null)?0:map.get(hd);
+        map.put(hd, psum+root.data);
+        verticalSumHelper(root.right, hd+1, map);
+    }
+   
+    static ArrayList <Integer> verticalOrder(Node root)
+    {
+        TreeMap<Integer,ArrayList<Integer>> mp = new TreeMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(root,0));
+       while (q.isEmpty()==false) {
+        Pair p=q.poll();
+        Node curr=p.node;
+        int hd=p.hd;
+        ArrayList<Integer> arr ;
+        if(mp.containsKey(hd)==false){
+            arr = new ArrayList<Integer>();
+        }else{
+            arr = mp.get(hd);
+        }
+            arr.add(curr.data);
+                mp.put(hd,arr);
+            if(curr.left!=null)
+                q.add(new Pair(curr.left,hd-1));
+            if(curr.right!=null)
+                q.add(new Pair(curr.right,hd+1));
+            
+       }
+        for (Map.Entry sum : mp.entrySet()) {
+        list.addAll((ArrayList<Integer>) sum.getValue());
+        }
+        return list;
+    }
+   
+    static ArrayList<Integer> topView(Node root)
+    {
+        // add your code
+        TreeMap<Integer,Integer> mp = new TreeMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(root,0));
+       while (q.isEmpty()==false) {
+        Pair p=q.poll();
+        Node curr=p.node;
+        int hd=p.hd;
+        if(mp.containsKey(hd)==false)
+                mp.put(hd,curr.data);
+            if(curr.left!=null)
+                q.add(new Pair(curr.left,hd-1));
+            if(curr.right!=null)
+                q.add(new Pair(curr.right,hd+1));
+            
+       }
+        for (Map.Entry sum : mp.entrySet()) {
+            list.add((int) sum.getValue());
+        }
+        return list;
+    }
+    public ArrayList <Integer> bottomView(Node root)
+    {
+        // Code here
+        TreeMap<Integer,Integer> mp = new TreeMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(root,0));
+       while (q.isEmpty()==false) {
+        Pair p=q.poll();
+        Node curr=p.node;
+        int hd=p.hd;
+                mp.put(hd,curr.data);
+            if(curr.left!=null)
+                q.add(new Pair(curr.left,hd-1));
+            if(curr.right!=null)
+                q.add(new Pair(curr.right,hd+1));
+            
+       }
+        for (Map.Entry sum : mp.entrySet()) {
+            list.add((int) sum.getValue());
+        }
+        return list;
     }
 }
