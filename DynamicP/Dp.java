0adcb9c7e2641ca525e9d3f7c9a0a38f0c5fa9de
shivamnanda436 <38239120+shivamnanda436@users.noreply.github.com>
2022-01-20 00:16:59 +0530

    dp continue

--- DynamicP/Dp.java
+++ DynamicP/Dp.java
@@ -294,6 +294,38 @@ public class Dp {
         }
         return max;
     }
+    public int maximizeCuts(int n, int x, int y, int z)
+    {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = -1;
+            if(i-x>=0){dp[i] = Math.max(dp[i], dp[i-x]);}
+            if(i-y>=0){dp[i] = Math.max(dp[i], dp[i-y]);}
+            if(i-z>=0){dp[i] = Math.max(dp[i], dp[i-z]);}
+            if(dp[i]!=-1){
+                dp[i]++;
+            }
+        }
+        return dp[n];
+
+    }
+    static int minJumps(int[] arr){
+        // your code here
+         int[] dp = new int[arr.length];
+        dp[0] = 0;
+        
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < i; j++) {
+                if(arr[j]+j >= i && dp[j] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[j]+1);
+                    break;
+                }
+            }
+        }
+        return dp[arr.length-1] == Integer.MAX_VALUE ? -1 :dp[arr.length-1];
+    }
         public static void main(String[] args) {
         System.out.println(findNthFibonacci(7));
        
