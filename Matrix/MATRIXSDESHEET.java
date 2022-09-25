e53682e8210b6506d4f43ed91cd62433a6d196d0
shivamnanda436 <38239120+shivamnanda436@users.noreply.github.com>
2022-09-25 22:37:28 +0530

    25-09-2022

--- Matrix/MATRIXSDESHEET.java
+++ Matrix/MATRIXSDESHEET.java
@@ -140,38 +140,140 @@ public class MATRIXSDESHEET {
         }
     }
 
-    static boolean existHelper(char[][] board, String word, int i, int j , int n, int m, int len){
-        if(word.length() <= len) return true;
-        if(word.length() == 0) return false;
-        if(i<0 || j<0 || i>=n || j>= m || board[i][j] == '.' || word.charAt(len) != board[i][j]) return false;
-        if( word.length() == 1 && word.charAt(0) == board[i][j]) return true;
+    static boolean existHelper(char[][] board, String word, int i, int j, int n, int m, int len) {
+        if (word.length() <= len)
+            return true;
+        if (word.length() == 0)
+            return false;
+        if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] == '.' || word.charAt(len) != board[i][j])
+            return false;
+        if (word.length() == 1 && word.charAt(0) == board[i][j])
+            return true;
 
         boolean temp = false;
         board[i][j] = '.';
-        int x[] = {0,0,-1,1};
-        int y[] = {-1,1,0,0};
+        int x[] = { 0, 0, -1, 1 };
+        int y[] = { -1, 1, 0, 0 };
 
         for (int k = 0; k < 4; k++) {
-            temp = temp || existHelper(board, word, i + x[k], j + y[k], n, m, len +1);
+            temp = temp || existHelper(board, word, i + x[k], j + y[k], n, m, len + 1);
         }
         board[i][j] = word.charAt(len);
         return temp;
     }
 
     public boolean exist(char[][] board, String word) {
-        if(board.length == 0) return false;
-        if( word.length() == 0) return false;
+        if (board.length == 0)
+            return false;
+        if (word.length() == 0)
+            return false;
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if(word.charAt(0) == board[i][j]){
-                    if(existHelper(board, word, i, j, board.length, board[i].length, 0)) return true;
+                if (word.charAt(0) == board[i][j]) {
+                    if (existHelper(board, word, i, j, board.length, board[i].length, 0))
+                        return true;
                 }
             }
         }
         return false;
     }
 
+    public static boolean isSafeNumOfIslands(char[][] grid, int ROW, int COL, int row, int col, boolean[][] visited) {
+        if (row < ROW && col < COL && row >= 0 && col >= 0 && !visited[row][col] && grid[row][col] == '1')
+            return true;
+        return false;
+    }
+
+    public static void DFS(char[][] grid, int ROW, int COL, int row, int col, boolean[][] visited) {
+        int[] rowDir = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] colDir = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        visited[row][col] = true;
+
+        for (int i = 0; i < 8; i++) {
+            if (isSafeNumOfIslands(grid, ROW, COL, row + rowDir[i], col + colDir[i], visited)) {
+                DFS(grid, ROW, COL, row + rowDir[i], col + colDir[i], visited);
+            }
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    DFS(grid, grid.length, grid[0].length, i, j, visited);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void floodUtil(char a[][], int ROW, int COL, int row, int col, char prev, char newv) {
+        // check base conditions
+        if (row >= ROW || col >= COL || row < 0 || col < 0)
+            return;
+
+        // check whether it is equal to -
+        if (prev != a[row][col])
+            return;
+
+        // set value to 'O'
+        a[row][col] = newv;
+
+        // call for south, nort, east ,west
+        floodUtil(a, ROW, COL, row + 1, col, prev, newv);
+        floodUtil(a, ROW, COL, row - 1, col, prev, newv);
+        floodUtil(a, ROW, COL, row, col - 1, prev, newv);
+        floodUtil(a, ROW, COL, row, col + 1, prev, newv);
+    }
+
+    static char[][] fill(int n, int m, char a[][]) {
+
+        // replace all O with -
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == 'O')
+                    a[i][j] = '-';
+            }
+        }
+
+        // call flodutil for edges
+
+        // left edge
+        for (int i = 0; i < n; i++) {
+            if (a[i][0] == '-')
+                floodUtil(a, n, m, i, 0, '-', 'O');
+        }
+        // top edge
+        for (int i = 0; i < m; i++) {
+            if (a[0][i] == '-')
+                floodUtil(a, n, m, 0, i, '-', 'O');
+        }
+        // right edge
+        for (int i = 0; i < n; i++) {
+            if (a[i][m - 1] == '-')
+                floodUtil(a, n, m, i, m - 1, '-', 'O');
+        }
+        // down edge
+        for (int i = 0; i < m; i++) {
+            if (a[n - 1][i] == '-')
+                floodUtil(a, n, m, n - 1, i, '-', 'O');
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == '-')
+                    a[i][j] = 'X';
+            }
+        }
+
+        return a;
+    }
 
     public static void main(String[] args) {
         int N = 3;
