373fc405a7f203b0a86982ad07e6f314ec49e305
iamshivamnanda <shivamnanda436@gmail.com>
2022-10-28 22:51:36 +0530

    28-10-2022

--- Backtracking/BackTrackingSDESheet.java
+++ Backtracking/BackTrackingSDESheet.java
@@ -481,6 +481,158 @@ public class BackTrackingSDESheet {
             temp = new ArrayList<>(curr);
         }
     }
+
+    public static boolean isKPartitionPossibleUtil(int a[], int n, int k, int subsetSum[], boolean visited[], int sum,
+            int curIndex, int limIndex) {
+        if (subsetSum[curIndex] == sum) {
+
+            if (curIndex == k - 2) {
+                return true;
+            }
+
+            return isKPartitionPossibleUtil(a, n, k, subsetSum, visited, sum, curIndex + 1, n - 1);
+        }
+
+        for (int i = limIndex; i >= 0; i--) {
+            if (visited[i])
+                continue;
+
+            int tem = subsetSum[curIndex] + a[i];
+            if (tem <= sum) {
+                visited[i] = true;
+                subsetSum[curIndex] = tem;
+                if (isKPartitionPossibleUtil(a, n, k, subsetSum, visited, sum, curIndex, i - 1)) {
+                    return true;
+                }
+                visited[i] = false;
+                subsetSum[curIndex] = tem - a[i];
+            }
+        }
+        return false;
+    }
+
+    public boolean isKPartitionPossible(int a[], int n, int k) {
+        if (k == 1)
+            return true;
+        if (n < k)
+            return false;
+
+        int sum = 0;
+        for (int no : a)
+            sum += no;
+
+        if (sum % k != 0)
+            return false;
+
+        int subsetSum[] = new int[k];
+        boolean visited[] = new boolean[n];
+        subsetSum[0] = a[n - 1];
+        visited[n - 1] = true;
+        int subSum = sum / k;
+
+        return isKPartitionPossibleUtil(a, n, k, subsetSum, visited, subSum, 0, n - 1);
+    }
+
+    public static void markadjacent(int[][] mat) {
+        int x[] = { 0, 0, 1, -1 };
+        int y[] = { 1, -1, 0, 0 };
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                // if it is a mine
+                if (mat[i][j] == 0) {
+                    for (int j2 = 0; j2 < 4; j2++) {
+                        if (isValidFSP2(mat, i + x[j2], j + y[j2]))
+                            mat[i + x[j2]][j + y[j2]] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean isValidFSP(int mat[][], boolean[][] visited, int i, int j) {
+        if (i < 0 || i >= mat.length || j < 0 || j >= mat[0].length || visited[i][j] || mat[i][j] == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidFSP2(int mat[][], int i, int j) {
+        if (i < 0 || i >= mat.length || j < 0 || j >= mat[0].length) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void findShortestPathUtil(int[][] mat, boolean[][] visited, int i, int j, int dist) {
+        if (j == mat[0].length - 1) {
+            min_dist = Math.min(min_dist, dist);
+            return;
+        }
+
+        if (dist > min_dist)
+            return;
+
+        int x[] = { 0, 0, 1, -1 };
+        int y[] = { 1, -1, 0, 0 };
+        visited[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            if (isValidFSP(mat, visited, i + x[k], j + y[k])) {
+                findShortestPathUtil(mat, visited, i + x[k], j + y[k], dist + 1);
+            }
+        }
+        visited[i][j] = false;
+
+    }
+
+    static int min_dist;
+
+    // find shortest path
+    public static int findShortestPath(int[][] mat) {
+        // mark all adjacent elements as unsafe
+        markadjacent(mat);
+
+        min_dist = Integer.MAX_VALUE;
+
+        // check for all rows
+        for (int i = 0; i < mat.length; i++) {
+            if (mat[i][0] == 1) {
+                boolean visited[][] = new boolean[mat.length][mat[0].length];
+                findShortestPathUtil(mat, visited, i, 0, 0);
+
+            }
+        }
+
+        return min_dist == Integer.MAX_VALUE ? -1 : min_dist;
+    }
+
+    boolean dfs(ArrayList<ArrayList<Integer>> adj, int u,int cnt,int N){
+        if(cnt==N) return true;
+        vis[u] = true;
+        for(int i:adj.get(u)){
+            if(!vis[i] && dfs(adj,i,cnt+1,N)) return true;
+        }
+        vis[u] = false;
+        return false;
+    }
+    boolean[] vis;
+    boolean check(int N, int M, ArrayList<ArrayList<Integer>> Edges) 
+    { 
+        vis = new boolean[N+1];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0;i<=N;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(ArrayList<Integer> i: Edges){
+            int u = i.get(0);
+            int v = i.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        for(int i = 1;i<N;i++){
+            if(dfs(adj,i,1,N)) return true;
+        }
+        return false;
+    }
 }
 
 class Pair {
